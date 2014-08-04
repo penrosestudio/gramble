6eeabffa5618745b441a6bc541df80c240954201
jayfresh <jonathan@penrosestudio.com>
2014-08-04 17:05:05 +0100

    Updating HTML structure of items

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -18,10 +18,17 @@
                     description = item.n,
                     timestamp = item.dt,
                     isImage = ['.jpg','.png','.gif'].indexOf(fileExtension) !== -1,
-                    itemContent = isImage ? '<a class="item image"><img src="'+url+'" alt="'+title+'"></div>' : '<div class="item article"><a href="'+url+'" title="'+title+'">'+title+'</a><p>'+description+'</p><span class="cover"><span class="openLink">open link</span></span></a>'
+                    itemContent = isImage ? '<a class="item image" href="'+url+' target="_blank"> \
+                        <img src="'+url+'" alt="'+title+'"> \
+                        <span class="cover"><span class="openLink">open link</span></span> \
+                    </a>' : '<a class="item article" href="'+url+'" title="'+title+'"> \
+                        <p class="title">'+title+'</p> \
+                        <p class="body">'+description+'</p> \
+                        <span class="cover"><span class="openLink">open link</span></span> \
+                    </a>';
                 $container.append(itemContent);
             });
-            // activate Masonry
+            // activate Packery
             var pckry = new Packery($container.get(0), {
                 itemSelector: '.item',
                 columnWidth: '.item'
