4cd16b1c1cc9a792ad04ba2d1b76bc7b334e33e9
jayfresh <jonathan@penrosestudio.com>
2014-08-04 12:44:03 +0100

    Templating the content items into a separate section of index.html

--- src/javascripts/app.js
+++ src/javascripts/app.js
@@ -9,13 +9,21 @@
         // wait until after the document is ready to render the data
         console.log('data!',data);
         $(document).ready(function() {
-            var $container = $('.grambleContainer');
+            // add the items to the container
+            var $container = $('.grambleContainer2');
             $(data).each(function(i, item) {
                 var title = item.d,
                     url = item.u,
+                    fileExtension = url.substring(url.lastIndexOf('.')),
                     description = item.n,
-                    timestamp = item.dt;
-                $container.append('<p>'+title+' - '+url+' - '+description+'</p>');
+                    timestamp = item.dt,
+                    isImage = ['.jpg','.png','.gif'].indexOf(fileExtension) !== -1,
+                    itemContent = isImage ? '<div class="item"><img src="'+url+'" alt="'+title+'"></div>' : '<div class="item article"><a href="'+url+'" title="'+title+'">'+title+'</a><p>'+description+'</p></div>'
+                $container.append(itemContent);
+            });
+            // activate Masonry
+            var msnry = new Masonry($container.get(0), {
+                itemSelector: '.item'
             });
         });
     }
